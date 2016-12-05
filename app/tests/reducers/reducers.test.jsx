64f19234eeb23a35287df41b0950b9dabf5dae84
johnnyqbui <johnnyqbui7@gmail.com>
2016-12-05 02:53:30 -0600

    add todos reducer and tests

--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -25,4 +25,36 @@ describe('Reducers', () => {
 			expect(res).toBe(true);
 		})
 	})
+
+	describe('todosReducer', () => {
+		it('should add new todo', () => {
+			const action = {
+				type: 'ADD_TODO',
+				text: 'complete testing'
+			}
+			const res = reducers.todosReducer(df([]), df(action));
+
+			expect(res.length).toEqual(1);
+			expect(res[0].text).toEqual(action.text);
+		})
+
+		it('should toggle todo', () => {
+			const todos = {
+				id: 1,
+				text: 'something',
+				completed: true,
+				createdAt: 1,
+				completedAt: 2
+			}
+
+			const action = {
+				type: 'TOGGLE_TODO',
+				id: 1
+			}
+
+			const res = reducers.todosReducer(df(todos), df(action));
+			expect(res[0].completed).toBe(false);
+			expect(res[0].completedAt).toBe('undefined');
+		})
+	})
 })
